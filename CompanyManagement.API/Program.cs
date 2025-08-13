00cfb3d5cec76fe530137959a34d1703fce8e17d
Tomas Sánchez Jimenez <tsanchez.accenture@cmpc.com>
2025-08-13 10:30:45 -0500

    initial commit

---
+++ CompanyManagement.API/Program.cs
@@ -0,0 +1,24 @@
+using CompanyManagement.API.Configurations;
+using CompanyManagement.API.Endpoints;
+using CompanyManagement.API.Middleware;
+using CompanyManagement.Infrastructure.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocumentation();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerDocumentation();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseHttpsRedirection();
+app.MapCompanyEndpoints();
+
+app.Run();
